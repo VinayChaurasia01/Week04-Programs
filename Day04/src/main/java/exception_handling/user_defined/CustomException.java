41ca3bd86b6a8de89ff3fedaed45d13ba478d5d0
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-02-14 15:09:38 +0530

    Vinay Add CustomException for make custom exceptions

---
+++ Day04/src/main/java/exception_handling/user_defined/CustomException.java
@@ -0,0 +1,25 @@
+package exception_handling.user_defined;
+
+public class CustomException {
+    public static void main(String[] args) {
+        try {
+            validateAge(15);
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught InvalidAgeException : " + e.getMessage());
+        }
+
+        try {
+            validateAge(25);
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught InvalidAgeException : " + e.getMessage());
+        }
+    }
+
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or older");
+        }
+        System.out.println("Age is valid");
+    }
+
+}
